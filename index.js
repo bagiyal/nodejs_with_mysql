8af082fa1b86c1363830c9d27fb7808a0f0fb315
Abhishek Bagiyal <63339447+bagiyal@users.noreply.github.com>
2024-01-08 21:05:43 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const mysql = require('mysql');
+const app  = express();
+const port = 3000;
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'password',
+    database: 'tcil-sotc',
+  });
+
+  connection.connect((err) =>{
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+    return;
+    }
+    console.error('Error connecting to MySQL:', err);
+  })
+
